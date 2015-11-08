0aa42af9276723d2f91f651ac591ddda3c2491db
Katsuyoshi Kojima <kk-pg@pg-box.jp>
2015-11-09 00:11:39 +0900

    update to .gitignore add testScript

---
+++ Assets/Script/PrefsControllTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefsControllTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.SetString ("testStr", "テスト文字列");
+        PlayerPrefs.SetInt("testInt", 111111);
+        PlayerPrefs.SetFloat("testFloat", 1.1111f);
+        PlayerPrefs.Save();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void onClickDispPrefs()
+    {
+        Debug.Log(PlayerPrefs.GetString("testStr"));
+        Debug.Log(PlayerPrefs.GetInt("testInt"));
+        Debug.Log(PlayerPrefs.GetFloat("testFloat"));
+    }
+}
